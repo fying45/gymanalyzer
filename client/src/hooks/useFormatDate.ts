8f4ab2f427443a5a75c82d7219c01c260d7f3ffa
fying45 <fabien.fock@gmail.com>
2024-11-01 16:25:14 +0100

    fix(client): date formatter

--- client/src/hooks/useFormatDate.ts
+++ client/src/hooks/useFormatDate.ts
@@ -5,7 +5,7 @@ type UseDateReturnType = {
 
 export const useDate = (): UseDateReturnType => {
   const formatDate = (date: Date): string => {
-    const day = String(date.getDay()).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
 
